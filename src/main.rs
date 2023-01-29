25a38c076a5f4529e966e83a11034a6f88dc4de7
Dylan Turner <dylantdmt@gmail.com>
2023-01-28 19:36:55 -0600

    main: create inital window
    Signed-off-by: Dylan Turner <dylantdmt@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,30 @@
  * Description: Entry point for application
  */
 
+use gtk::{
+    Application, ApplicationWindow,
+    prelude::{
+        ApplicationExtManual, ApplicationExt, WidgetExt
+    }
+};
+
+mod pkg;
+
 fn main() {
-    println!("Hello, world!");
+    let app = Application::builder()
+        .application_id("blueOkiris.github.Aipster")
+        .build();
+
+    app.connect_activate(|app| {
+        let win = ApplicationWindow::builder()
+            .application(app)
+            .title("Aipster")
+            .default_width(800)
+            .default_height(600)
+            .build();
+
+        win.show_all();
+    });
+
+    app.run();
 }
