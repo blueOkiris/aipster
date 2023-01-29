735695c61c1e4a561efb20d1f8fb9ab8b04598a6
Dylan Turner <dylantdmt@gmail.com>
2023-01-28 22:09:30 -0600

    main: basic package list system
    I created the first set of organization for package display.
    Planning to have an image, the package info, and then buttons on the
    side.
    
    Various scroll windows are used to show packages
    
    Signed-off-by: Dylan Turner <dylantdmt@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,22 @@
  * Description: Entry point for application
  */
 
+mod pkg;
+
 use gtk::{
     Application, ApplicationWindow,
+    Box, ScrolledWindow, Label, Button,
+    Align, PolicyType, Orientation,
     prelude::{
-        ApplicationExtManual, ApplicationExt, WidgetExt
-    }
+        ApplicationExtManual, ApplicationExt,
+        WidgetExt, ContainerExt, BoxExt
+    }, Adjustment
+};
+use crate::pkg::{
+    pull_package_list, Package
 };
 
-mod pkg;
+const MARGIN: i32 = 5;
 
 fn main() {
     let app = Application::builder()
@@ -23,10 +31,102 @@ fn main() {
             .title("Aipster")
             .default_width(800)
             .default_height(600)
+            .expand(true).hexpand(true).vexpand(true)
             .build();
+
+        let pkg_disp = create_pkg_display();
+        win.add(&pkg_disp);
 
         win.show_all();
     });
 
     app.run();
+}
+
+/// Create a list of packages in a vertically scrolled box
+fn create_pkg_display() -> Box {
+    // Main, expanding container
+    let cont = Box::builder()
+        .margin(0).hexpand(true).vexpand(true)
+        .build();
+
+    // A scrollbar bc the item count will be large
+    let scroll_cont = ScrolledWindow::builder()
+        .margin(MARGIN).hexpand(true).vexpand(true)
+        .hscrollbar_policy(PolicyType::Never).vscrollbar_policy(PolicyType::Always)
+        .build();
+
+    // An inner container for all the items that will be scollable
+    let pkg_disp = Box::builder()
+        .orientation(Orientation::Vertical)
+        .margin(0).hexpand(true).vexpand(true)
+        .build();
+
+    // Add each package thing to the main vbox
+    for pkg in pull_package_list() {
+        let pkg_view = create_pkg_view(&pkg);
+        pkg_disp.pack_start(&pkg_view, true, true, MARGIN as u32);
+    }
+
+    scroll_cont.add(&pkg_disp);
+    cont.pack_start(&scroll_cont, true, true, 0);
+    cont
+}
+
+// Turn package data into a graphical view
+fn create_pkg_view(pkg: &Package) -> Box {
+    // Main container of each item
+    let cont = Box::builder()
+        .margin(0).hexpand(true).vexpand(true)
+        .build();
+
+    // Hor scroll for long desc
+    let scroll_cont = ScrolledWindow::builder()
+        .margin(MARGIN).hexpand(true).vexpand(true)
+        .hscrollbar_policy(PolicyType::Automatic).vscrollbar_policy(PolicyType::Never)
+        .build();
+    
+    /*
+     * Actual container of info
+     *
+     * ----------------------------------------------
+     * |          | Name: name                      |
+     * |   Image  | Description: blah blah blah ... |
+     * |          | ...                             |
+     * |__________|_________________________________|
+     */
+    let entry = Box::builder()
+        .orientation(Orientation::Horizontal)
+        .margin(0).hexpand(true).vexpand(true)
+        .build();
+
+    let pic = Label::new(Some("PIC HERE"));
+    
+    let info_box = Box::builder()
+        .orientation(Orientation::Vertical)
+        .margin(0).hexpand(true).vexpand(true)
+        .build();
+
+    let name_box = Box::builder()
+        .orientation(Orientation::Horizontal).halign(Align::Start)
+        .margin(0).hexpand(true).vexpand(true)
+        .build();
+    name_box.pack_start(&Label::new(Some("Name: ")), true, true, 0);
+    name_box.pack_start(&Label::new(Some(&pkg.name.clone())), true, true, 0);
+    let desc_box = Box::builder()
+        .orientation(Orientation::Horizontal).halign(Align::Start)
+        .margin(0).hexpand(true).vexpand(true)
+        .build();
+    desc_box.pack_start(&Label::new(Some("Description: ")), true, true, 0);
+    desc_box.pack_start(&Label::new(Some(&pkg.description.clone())), true, true, 0);
+
+    info_box.pack_start(&name_box, true, true, 0);
+    info_box.pack_start(&desc_box, true, true, 0);
+
+    entry.pack_start(&pic, true, true, 0);
+    entry.pack_start(&info_box, true, true, 0);
+
+    scroll_cont.add(&entry);
+    cont.pack_start(&scroll_cont, true, true, 0);
+    cont
 }
