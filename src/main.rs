82ae12a6943a7f4471959be4de7d1a9163880fc4
Dylan Turner <dylantdmt@gmail.com>
2023-01-29 00:10:24 -0600

    main: implement rebuild
    You can refresh and toggle installed-only/all.
    If installed-only is toggled, it will stay toggled after refresh
    
    Signed-off-by: Dylan Turner <dylantdmt@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,14 @@ use gtk::{
     Align, PolicyType, Orientation,
     prelude::{
         ApplicationExtManual, ApplicationExt,
-        WidgetExt, ContainerExt, BoxExt
+        WidgetExt, ContainerExt, BoxExt, ButtonExt, ToggleButtonExt, WidgetExtManual
     }
 };
-use reqwest::blocking::get;
 use webkit2gtk::{
     WebView,
     traits::WebViewExt
 };
+use reqwest::blocking::get;
 use crate::pkg::{
     pull_package_list, get_pkg_manifest, Package
 };
@@ -30,6 +30,9 @@ const BAR_HEIGHT: i32 = 20;
 const MIN_SEARCH_BAR_LEN: i32 = 100;
 const ICON_SIZE: i32 = 80;
 const BUTTON_SIZE: i32 = 64;
+
+static mut WIN: Option<ApplicationWindow> = None;
+static mut PANEL: Option<Box> = None;
 
 fn main() {
     let app = Application::builder()
@@ -44,18 +47,13 @@ fn main() {
             .default_height(600)
             .expand(true).hexpand(true).vexpand(true)
             .build();
-
-        let top_bar = create_top_bar();
-        let pkg_disp = create_pkg_display();
-
-        let panel = Box::builder()
-            .orientation(Orientation::Vertical)
-            .hexpand(true).vexpand(true)
-            .margin(MARGIN)
-            .build();
-        panel.pack_start(&top_bar, false, false, 0);
-        panel.pack_end(&pkg_disp, true, true, 0);
+        let panel = create_win_content(false);
         win.add(&panel);
+
+        unsafe {
+            WIN = Some(win.clone());
+            PANEL = Some(panel.clone());
+        }
 
         win.show_all();
     });
@@ -63,8 +61,48 @@ fn main() {
     app.run();
 }
 
+/// Use global state to redo the window creation
+fn refresh_window(installed_only: bool) {
+    let win = unsafe {
+        WIN.clone().unwrap()
+    };
+    let panel = unsafe {
+        PANEL.clone().unwrap()
+    };
+    win.remove(&panel);
+    unsafe {
+        panel.destroy();
+    }
+
+    let new_panel = create_win_content(installed_only);
+    win.add(&new_panel);
+    
+    unsafe {
+        WIN = Some(win.clone());
+        PANEL = Some(new_panel.clone());
+    }
+
+    win.show_all();
+}
+
+/// Put top bar and package list into a container
+fn create_win_content(installed_only: bool) -> Box {
+    let top_bar = create_top_bar(installed_only);
+    let pkg_disp = create_pkg_display(installed_only);
+
+    let panel = Box::builder()
+        .orientation(Orientation::Vertical)
+        .hexpand(true).vexpand(true)
+        .margin(MARGIN)
+        .build();
+    panel.pack_start(&top_bar, false, false, 0);
+    panel.pack_end(&pkg_disp, true, true, 0);
+
+    panel
+}
+
 /// Create a list of packages in a vertically scrolled box
-fn create_pkg_display() -> Box {
+fn create_pkg_display(installed_only: bool) -> Box {
     // Main, expanding container
     let cont = Box::builder()
         .margin(0).hexpand(true).vexpand(true)
@@ -85,6 +123,9 @@ fn create_pkg_display() -> Box {
     // Add each package thing to the main vbox
     let inst_pkgs = get_pkg_manifest();
     for pkg in pull_package_list() {
+        if installed_only && !inst_pkgs.iter().any(|e| e.name == pkg.name) {
+            continue;
+        }
         let pkg_view = create_pkg_view(&pkg, inst_pkgs.iter().find(|e| e.name == pkg.name));
         pkg_disp.pack_start(&pkg_view, true, true, MARGIN as u32);
     }
@@ -233,7 +274,7 @@ fn create_pkg_view(pkg: &Package, inst_pkg: Option<&Package>) -> Box {
 }
 
 /// Create a top bar with a search box, installed checkbox, and refresh
-fn create_top_bar() -> Box {
+fn create_top_bar(installed_only: bool) -> Box {
     let cont = Box::builder()
         .hexpand(true).vexpand(false).height_request(BAR_HEIGHT)
         .orientation(Orientation::Horizontal)
@@ -250,14 +291,22 @@ fn create_top_bar() -> Box {
 
     let installed = CheckButton::builder()
         .label("Installed")
+        .active(installed_only)
         .hexpand(false).vexpand(false)
         .margin_end(MARGIN)
         .build();
+    installed.connect_toggled(|inst| {
+        refresh_window(inst.is_active());
+    });
 
     let refresh_button = Button::builder()
         .label("⟲")
         .hexpand(false).vexpand(false)
         .build();
+    let installed_ref_btn_clone = installed.clone();
+    refresh_button.connect_clicked(move |_| {
+        refresh_window(installed_ref_btn_clone.is_active());
+    });
 
     cont.pack_end(&refresh_button, false, false, 0);
     cont.pack_end(&installed, false, false, 0);
